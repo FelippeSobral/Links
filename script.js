d642115e8601a18a41a25cbbdadd9e8dd2e5282a
FelippeSobral <lippesobral@hotmail.com>
2023-04-15 20:54:18 -0300

    parábens

---
+++ script.js
@@ -0,0 +1,17 @@
+function togglemode() {
+
+    const html = document.documentElement
+
+    if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }
+    
+   /*html.classList.toggle("light")*/
+
+     
+
+
+}
+
